03d18a993cad9fb03c442eb97eda7f1d1bfa3495
dargem <genericz145@gmail.com>
2026-08-11 12:39:35 +1000

    fix up locking and add dur

--- spam_buffer.py
+++ spam_buffer.py
@@ -5,19 +5,23 @@ from threading import Lock
 # Maintains a queue with a given max size. Elements > 1m get deleted. 
 # If the queue is full you cannot add to it. Adding to a full queue is considered "spams"
 class SpamBuffer:
-    def __init__(self, max_req_m = 5):
-        # Maps username -> deque[floats of call time, got by time.time()]
-        self.calls: defaultdict[str, deque[float]] = defaultdict(lambda: deque(maxlen=max_req_m))
-        self.locks: defaultdict[str, Lock] = defaultdict(Lock)
+    # max_req per dur, managed by a queue
+    def __init__(self, max_req = 4, dur = 40):
+        # Maps username -> deque[floats of call time, got by time.monotonic()]
+        self.calls: defaultdict[str, deque[float]] = defaultdict(lambda: deque(maxlen=max_req))
+        self.dur = dur
+
+        # Global lock for all names, can change this to be name level to get a more fine grained version if needed
+        self.lock = Lock() 
 
     # Try to add a name to the spam buffer, if adding fails this is considered "spam"
     def add(self, name: str) -> bool:
-        current = time.time() # Current time in seconds
+        current = time.monotonic() # Current time in seconds
 
-        with self.locks[name]:
+        with self.lock:
             dq = self.calls[name]
             while dq: # Falsy if empty
-                if dq[0] + 60 >= current:
+                if dq[0] + self.dur >= current:
                     break # Early ret if < 1m old
 
                 dq.popleft()
