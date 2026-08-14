6025aafb656ce5cd5d3357dcd6318ee884878e9e
dargem <genericz145@gmail.com>
2026-08-14 10:57:19 +1000

    pied piper TTS

--- TTS.py
+++ TTS.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from piper import PiperVoice, SynthesisConfig
+import wave
 
 class AbstractTTS(ABC):
     @abstractmethod
@@ -21,6 +22,10 @@ class PiedPierTTS(AbstractTTS):
         self.config = syn_config
         self.voice = voice
 
-    def generate_text(text: str, path: Path):
-        with 
-    
+    def generate_text(self, text: str, path: Path):
+        with wave.open(path.name, "wb") as wav_f:
+            self.voice.synthesize_wav(
+                text,
+                wav_f,
+                syn_config=self.config
+            )
