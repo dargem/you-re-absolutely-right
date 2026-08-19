993f695f307197be28ed32f7fc2ad56e9f763009
dargem <genericz145@gmail.com>
2026-08-20 09:47:18 +1000

    reinstate the identifier for caching, kinda sketch

--- TTS.py
+++ TTS.py
@@ -14,9 +14,9 @@ class AbstractTTS(ABC):
 
     # Get identifier for model name so we know what we are "dealing with" for caching
     # In practice can just be the name of the model + config options
-    # @abstractmethod
-    # def get_model_id(self) -> str:
-    #     pass
+    @abstractmethod
+    def get_model_id(self) -> str:
+        pass
 
 
 class PiedPierTTS(AbstractTTS):
@@ -40,6 +40,10 @@ class PiedPierTTS(AbstractTTS):
                 syn_config=self.config
             )    
 
+    def get_model_id(self):
+        # Repr can be used to get something like a "hash", kindof sketchy but can use for now
+        return "Pied_Piper" + repr(self.config)
+
 load_dotenv()
 
 # Ensure your API key has Text to Speech enabled for endpoints
@@ -48,13 +52,19 @@ class ElevenLabsTTS(AbstractTTS):
         self, client: ElevenLabs = ElevenLabs(api_key=os.getenv("ELEVEN_LABS_API"))
     ):
         self.client = client
+        self.voice_id=os.getenv("ELEVEN_LABS_VOICE_ID"),
+        self.model_id="eleven_multilingual_v2"
 
     def generate_text(self, text: str, path: Path):
         # On the free plan only default voices
         audio = self.client.text_to_speech.stream(
             text=text,
-            voice_id=os.getenv("ELEVEN_LABS_VOICE_ID"),
-            model_id="eleven_multilingual_v2"
+            voice_id=self.voice_id,
+            model_id=self.model_id
         )
 
         save(audio, str(path))
+
+    def get_model_id(self):
+        # May need to include model settings temp stuff
+        return f"ElevenLabs{self.voice_id}{self.model_id}"
